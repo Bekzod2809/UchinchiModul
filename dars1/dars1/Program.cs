e046e3847401ff3deb211994fae5984aeaa2c4f1
Bekzod <bekzodjonxasanov7@gmail.com>
2026-02-03 14:41:37 +0500

    added

---
+++ dars1/dars1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace dars1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MyList myList = new MyList(3);
+        myList.Add(77);
+        myList.Add(71);
+        myList.Add(73);
+        myList.Add(75);
+        myList.Add(77);
+        myList.RemoveAll(77);
+        myList.DisplayElements();
+    }
+}
