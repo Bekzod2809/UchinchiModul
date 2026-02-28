19a3270cc99bbe47d294c1dfb77092ed808af46b
Bekzod <bekzodjonxasanov7@gmail.com>
2026-02-28 14:00:13 +0500

    Added

---
+++ dars8/dars8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace dars8;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MyList<string> myList = new MyList<string>();
+        myList.Add("Hello");
+        myList.Add("World");
+        myList.DisplayElements();
+        MyList<int> myList2 = new MyList<int>();
+        myList2.Add(1);
+        myList2.Add(2);
+        myList2.DisplayElements();
+    }
+}
